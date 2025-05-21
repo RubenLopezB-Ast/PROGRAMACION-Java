ad1647e0c174bbbdfa807c6e085d9afcd2c91923
RubenLopezB-Ast <rlb09@hotmail.es>
2025-05-22 01:50:11 +0200

    Subida mapas y matrices

---
+++ src/mapas/src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, Double > notas = new HashMap<>();
+
+        notas.put("Ana", 8.5);
+        notas.put("Ana", 7.0); //Las claves deben de ser únicas si se repiten ("Ana") coge la última.
+        notas.put("Luis", 5.0);
+        notas.put("Pepe", 9.0);
+            //for each par recorrer el mapa.
+        for (Map.Entry<String,Double> entrada : notas.entrySet()){
+            System.out.println(entrada.getKey()+ "->"+entrada.getValue());
+        }
+        System.out.println("Introduce nombre: ");
+        String nombre = scanner.nextLine();
+
+        if(notas.containsKey(nombre)){
+            System.out.println(nombre + "tiene una notas de: "+ notas.get(nombre));
+        }else {
+            System.out.println("Estudiante no encontrado");
+        }
+    }
+}
